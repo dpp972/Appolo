a1485d54a6e98ac1581fe31c95a4f75b3c07ae17
john <john.djearamin@gmail.com>
2014-07-02 11:34:27 +0200

    ajouter des template

--- src/Appolo/FrontVitrineBundle/Controller/DefaultController.php
+++ src/Appolo/FrontVitrineBundle/Controller/DefaultController.php
@@ -9,11 +9,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
    /**
-     * @Route("/accueil")
+     * @Route("/accueil" ,name="accueil")
      * @Template()
      */
     public function indexAction()
     {
          return $this->render('AppoloFrontVitrineBundle:Default:index.html.twig');
+    }
+    
+    /**
+     * @Route("/register" ,name="register")
+     * @Template()
+     */
+    public function registerAction()
+    {
+         return $this->render('AppoloFrontVitrineBundle:Default:register.html.twig');
+    }
+    
+    /**
+     * @Route("/login" ,name="login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+         return $this->render('AppoloFrontVitrineBundle:Default:login.html.twig');
+    }
+    
+    /**
+     * @Route("/contact" ,name="contact")
+     * @Template()
+     */
+    public function contactAction()
+    {
+         return $this->render('AppoloFrontVitrineBundle:Default:contact.html.twig');
     }
 }
